e4dfa6774048fd6f5894a27fa77c1426d2ebc43f
DavidKogler <DavidKogler@users.noreply.github.com>
2019-05-08 11:45:45 +0200

    Kogler 08.05 11:45
    - Vorläufige Gui Änderungen
    - Wegen Merge hochgeladen

---
+++ DriversGuide/DriversGuideApp.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DriversGuide
+{
+    partial class DriversGuideApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.btnOverview = new System.Windows.Forms.Button();
+            this.btnGPS = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.pnlSideBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlSideBar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.pnlSideBar.Controls.Add(this.panel1);
+            this.pnlSideBar.Controls.Add(this.btnOverview);
+            this.pnlSideBar.Controls.Add(this.btnGPS);
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(194, 450);
+            this.pnlSideBar.TabIndex = 31;
+            // 
+            // btnOverview
+            // 
+            this.btnOverview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnOverview.FlatAppearance.BorderSize = 0;
+            this.btnOverview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOverview.Location = new System.Drawing.Point(0, 116);
+            this.btnOverview.Name = "btnOverview";
+            this.btnOverview.Size = new System.Drawing.Size(194, 29);
+            this.btnOverview.TabIndex = 33;
+            this.btnOverview.Text = "Überblick";
+            this.btnOverview.UseVisualStyleBackColor = true;
+            this.btnOverview.Click += new System.EventHandler(this.btnOverview_Click);
+            // 
+            // btnGPS
+            // 
+            this.btnGPS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnGPS.FlatAppearance.BorderSize = 0;
+            this.btnGPS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGPS.Location = new System.Drawing.Point(0, 81);
+            this.btnGPS.Name = "btnGPS";
+            this.btnGPS.Size = new System.Drawing.Size(194, 29);
+            this.btnGPS.TabIndex = 32;
+            this.btnGPS.Text = "GPS";
+            this.btnGPS.UseVisualStyleBackColor = true;
+            this.btnGPS.Click += new System.EventHandler(this.btnGPS_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.Black;
+            this.lblHeader.Location = new System.Drawing.Point(3, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(189, 29);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Drivers Guide";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.lblHeader);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 41);
+            this.panel1.TabIndex = 32;
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Location = new System.Drawing.Point(194, 0);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(800, 450);
+            this.pnlContent.TabIndex = 32;
+            // 
+            // DriversGuideApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 450);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.pnlSideBar);
+            this.Name = "DriversGuideApp";
+            this.Text = "DriversGuideApp";
+            this.pnlSideBar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Button btnOverview;
+        private System.Windows.Forms.Button btnGPS;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Panel pnlContent;
+    }
+}
